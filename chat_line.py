dffe8857dbc129653de03b26254afc80a4fdf549
Buapailin <90301405+Pxon03@users.noreply.github.com>
2025-01-30 19:19:04 +0700

    Update chat_line.py

--- chat_line.py
+++ chat_line.py
@@ -17,7 +17,7 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 LINE_ACCESS_TOKEN = os.getenv("LINE_ACCESS_TOKEN")
 LINE_CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET")
 ADMIN_USER_ID = os.getenv("bplpoon")  # LINE User ID ของผู้จัดการ
-GOOGLE_SHEETS_CREDENTIALS = os.getenv("credentials/meta-vista-446710-b6-d2f76e23ec67.json.")  # ใส่ Path ไฟล์ JSON Credentials
+GOOGLE_SHEETS_CREDENTIALS = credentials/meta-vista-446710-b6-d2f76e23ec67.json # ใส่ Path ไฟล์ JSON Credentials
 
 # ตรวจสอบค่าที่ต้องใช้
 print(OPENAI_API_KEY, LINE_ACCESS_TOKEN, LINE_CHANNEL_SECRET, ADMIN_USER_ID, GOOGLE_SHEETS_CREDENTIALS)  # ตรวจสอบค่าตัวแปร
